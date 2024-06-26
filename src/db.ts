83610ba8ab8c35854a53258e38108ac4937cff65
Bima Putra <bimaatp.23@gmail.com>
2024-06-26 15:00:44 +0700

    Create db pool

---
+++ src/db.ts
@@ -0,0 +1,17 @@
+import dotenv from 'dotenv'
+import mysql from 'mysql2/promise'
+
+dotenv.config()
+
+const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  port: process.env.DB_POST as unknown as number,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
+})
+
+export default pool
